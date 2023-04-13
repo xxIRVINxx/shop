13a5af24c0e8d12f859352f3e4141e1bfd7aa2ab
John Irvin Panganiban <jipanganiban@gbox.adnu.edu.ph>
2023-04-13 19:44:23 +0800

    First commit

---
+++ resources/views/components/product-list.blade.php
@@ -0,0 +1,17 @@
+<div>
+<div class="container-fluid py-5">
+    <div class="col-12 mx-auto">
+        <h1 class="text-center">Our Products</h1>
+        <div class="row row-cols-4 gap-5 justify-content-center">
+            <div class="card col p-0">
+                <img src="{{asset('img/fab.png')}}" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h4 class="card-title">Classic Denim</h4>
+                    <p class="card-text">P499.00</p>
+                    <a href="#" class="btn-dark-green text-decoration-none">Buy now</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+</div>
